24b69227f83de9056e51d8524ca4139fadca2a7e
Rajdip27 <srajdip920@gmail.com>
2023-09-20 16:50:54 +0600

    Data List

---
+++ ListData/Program.cs
@@ -0,0 +1,5 @@
+﻿using ListData.model;
+Student student=new Student();
+Console.WriteLine($"My Id is : {student.Id}");
+Console.WriteLine($"My Name is : {student.Name}");
+Console.WriteLine($"My Description is : {student.Description}");
---
+++ ListData/model/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace ListData.model;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; }=string.Empty;
+    public Student()
+    {
+        Id = 1;
+        Name = "Rajdip";
+        Description = "Asp Dot Net";
+    }
+}
