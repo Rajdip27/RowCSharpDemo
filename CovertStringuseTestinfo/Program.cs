97bcdefd2153a9d7542a0d56e76bfa99310a9468
Rajdip27 <srajdip920@gmail.com>
2023-09-21 12:17:14 +0600

    Convert string use textInfo

---
+++ CovertStringuseTestinfo/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Globalization;
+
+string input = "this is a sample string";
+TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+string titleCase = textInfo.ToTitleCase(input);
+Console.WriteLine(titleCase);//output :This Is A Sample String
+Console.ReadKey(true);
--- ListData/Program.cs
+++ ListData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ListData.model;
 Student student=new Student();
-Console.WriteLine($"My Id is : {student.Id}");
-Console.WriteLine($"My Name is : {student.Name}");
-Console.WriteLine($"My Description is : {student.Description}");
+//Console.WriteLine($"My Id is : {student.Id}");
+//Console.WriteLine($"My Name is : {student.Name}");
+//Console.WriteLine($"My Description is : {student.Description}");
+Console.ReadKey(true);
