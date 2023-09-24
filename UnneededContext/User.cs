66d4b8ce7aa05943404f92892a6f55c5554a3f35
Rajdip27 <srajdip920@gmail.com>
2023-09-24 15:30:12 +0600

    UnneededContext

---
+++ UnneededContext/User.cs
@@ -0,0 +1,18 @@
+﻿namespace UnneededContext;
+
+public class User
+{
+    public User()
+    {
+        new User
+        {
+            Id = 1,
+            Name = "Raja",
+            Email = "Raja@gmail.com"
+        };
+    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; }= string.Empty;
+    public override string ToString()=> $"Id={Id},Name={Name},Email={Email}";
+}
