0085d8a0d0d0d928b04b1c842f990db1a4c19a7a
Rajdip27 <srajdip920@gmail.com>
2023-10-28 11:52:47 +0600

    C# Operators

---
+++ C#Operators/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Enter First Number : ");
+double Num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter First Number : ");
+double Num2 = Convert.ToDouble(Console.ReadLine());
+double Result = Num1 + Num2;
+Console.WriteLine("------------------------------------------------------------------------- ");
+Console.WriteLine($"The ans is : {Result}");
+Console.ReadKey(true);
