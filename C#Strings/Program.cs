dd6ab04fd1c98b33c835b788177b47c37451fee6
Rajdip27 <srajdip920@gmail.com>
2023-10-28 12:26:35 +0600

    C# Strings

---
+++ C#Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿//string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//Console.Write($"String Length : "+txt.Length);
+
+string Name = "Hridoy";
+Console.WriteLine(Name.ToLower());
+Console.WriteLine(Name.ToUpper());
+Console.ReadKey(true);
