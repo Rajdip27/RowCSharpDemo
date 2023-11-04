c353bd7506fdc40c66187aaf0886a7b5a1b723d1
Rajdip27 <srajdip920@gmail.com>
2023-11-04 16:25:38 +0600

    TWO DIMONSON ARRAY

---
+++ ForeacheLoop/Program.cs
@@ -0,0 +1,8 @@
+﻿int[]number={1,2,3,4,5,6,7};
+
+foreach (var item in number)
+{
+    Console.WriteLine($"Number Is :{item}");
+}
+Console.ReadKey(true);
+
---
+++ TWODIMONSONARRAY/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter the number of rows: ");
+int numRows = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number of columns: ");
+int numColumns = int.Parse(Console.ReadLine());
+
+int[,] twoDimArray = new int[numRows, numColumns];
+
+Console.WriteLine("Enter the elements of the 2D array:");
+
+// Input values into the 2D array
+for (int i = 0; i < numRows; i++)
+{
+    for (int j = 0; j < numColumns; j++)
+    {
+        Console.Write($"Enter element at row {i}, column {j}: ");
+        twoDimArray[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("2D Array Contents:");
+
+// Display the contents of the 2D array
+for (int i = 0; i < numRows; i++)
+{
+    for (int j = 0; j < numColumns; j++)
+    {
+        Console.Write(twoDimArray[i, j] + " ");
+    }
+    Console.WriteLine(); // Move to the next row
+}
