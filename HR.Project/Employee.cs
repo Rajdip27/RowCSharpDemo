516de89f5a162e66af583e7b0b1992b1c662ef58
Rajdip27 <srajdip920@gmail.com>
2023-11-04 16:08:49 +0600

    add Sort Project

---
+++ HR.Project/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace HR.Project;
+
+public class Employee:Person
+{
+    public Employee()
+    {
+        
+    }
+
+    public Employee(double basic, double pf, double ta, double hr)
+    {
+        HR = hr;
+        PF = pf;
+        Basic = basic;
+        TA = ta;
+    }
+    public string Designation { get; set; }
+    public string Department { get; set; }
+    private double HR { get; set; }
+    private double PF { get; set; }
+    private double TA { get; set; }
+    private double Basic { get; set; }
+
+    public double Salary()
+    {
+        return HR- PF + Basic + TA;
+    }
+}
---
+++ HR.Project/Program.cs
@@ -0,0 +1,19 @@
+﻿ using HR.Project;
+
+ Employee emp = new Employee(456454545,2000,1000,500);
+emp.Id=1001;
+emp.Name="Raja";
+emp.Phone="413145415";
+emp.Email="aba@Gmail.com";
+emp.Department="IT";
+emp.Designation="Hr";
+
+Console.Write(emp.Salary());
+//Console.Write(emp.Id);
+//Console.Write(emp.Name);
+//Console.Write(emp.Phone);
+//Console.Write(emp.Email);
+//Console.Write(emp.Department);
+//Console.Write(emp.Designation);
+
+Console.ReadKey(true);
