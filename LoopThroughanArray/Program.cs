077adbbefac73c7b4d7e8b38b54efa818fbc1402
Rajdip27 <srajdip920@gmail.com>
2023-11-20 10:55:47 +0600

    Multidimensional Arrays

---
+++ LoopThroughanArray/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+//for (int i = 0; i < cars.Length; i++)
+//{
+//    Console.WriteLine(cars[i]);
+//}
+
+Array.Sort(cars);
+foreach (var item in cars)
+{
+    Console.WriteLine(item);
+}
+
+
---
+++ MultidimensionalArrays/Program.cs
@@ -0,0 +1,16 @@
+﻿//int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+//for (int i = 0; i < numbers.GetLength(0); i++)
+//{
+//    for (int j = 0; j < numbers.GetLength(1); j++)
+//    {
+//        Console.WriteLine(numbers[i, j]);
+//    }
+//}
+
+int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+}
