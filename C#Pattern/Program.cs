27242c52d274800e8492421fb94694ea66a97748
Rajdip27 <srajdip920@gmail.com>
2023-11-22 17:43:58 +0600

    Input Samples

---
+++ C#Pattern/Program.cs
@@ -0,0 +1,48 @@
+﻿//using System.Threading.Channels;
+
+//for (int i = 0; i < 5; i++)
+//{
+
+//    for (int j = 0; j < 5; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.ReadKey(true);
+
+//Console.Write("Enter Your Last Value : ");
+//double num = Convert.ToDouble(Console.ReadLine());
+
+//double result = 0;
+
+//for (int i = 0; i < num; i++)
+//{
+//    if (i % 2 != 0)
+//    {
+//        result += i;
+//    }
+//}
+//Console.Write($"The Ans is {result}");
+
+//Console.WriteLine("Enter the radius (R):");
+//double radius = double.Parse(Console.ReadLine());
+
+//// Calculate the area using the formula A = π * R^2
+//double area = Math.PI * Math.Pow(radius, 2);
+
+//// Print the result with four decimal places
+//Console.WriteLine("A=" + area.ToString("F4"));
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    for (int j = 5; j >=i; j--)
+//    {
+//        Console.Write("*");
+//    }
+
+//    Console.WriteLine();
+//}
+
+//Console.ReadKey();
--- StringConcatenation/Program.cs
+++ StringConcatenation/Program.cs
@@ -1,11 +1,28 @@
-﻿string fastName = "Raza";
-string lastName = "Raj";
-string fullName = string.Concat( fastName+" " + lastName);
-Console.WriteLine($"My Name is {fullName}");
-Console.ReadKey();
+﻿//string fastName = "Raza";
+//string lastName = "Raj";
+//string fullName = string.Concat( fastName+" " + lastName);
+//Console.WriteLine($"My Name is {fullName}");
+//Console.ReadKey();
 
 
+//int[,] numbers = { { 1, 4, 2 }, { 3, 6,
+//    8 } };
+//for (int i = 0; i <
+//                numbers.GetLength(0); i++)
+//{
+//    for (int j = 0; j <
+//                    numbers.GetLength(1); j++)
+//    {
+//        Console.WriteLine(numbers[i,
+//            j]);
+//    }
+//}
 
+int x = 5;
+Console.WriteLine("Initial value of x: " + x);
+// Using ^= operator to perform bitwise XOR with 3
+x ^= 3;
+Console.WriteLine("After x ^= 3, the new value of x: " + x);
 
 
 
