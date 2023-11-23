eae8d77b318fc03e2be111cc10dc618e80e9dda2
Rajdip27 <srajdip920@gmail.com>
2023-11-23 15:48:39 +0600

    Pattern Add

---
+++ PatternC#/Program.cs
@@ -0,0 +1,38 @@
+﻿//******
+//******
+//******
+//******//output
+//******
+//******
+
+
+//for (int i = 0; i <= 5; i++)
+//{
+//    for (int j = 0; j <=5; j++)
+//    {
+//        Console.Write("*");
+//    }
+
+//    Console.WriteLine();
+//}
+
+//Console.ReadKey();
+
+
+//111111
+//222222
+//333333//output
+//444444
+//555555
+
+
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 0; j <=5; j++)
+    {
+        Console.Write(i);
+    }
+    Console.WriteLine();
+}
+Console.ReadKey(true);
+
