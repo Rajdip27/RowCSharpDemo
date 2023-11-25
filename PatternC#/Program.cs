5846cbd9f1ee6c6483f86f18e7720ce1dca4975b
Rajdip27 <srajdip920@gmail.com>
2023-11-25 17:17:36 +0600

    Looping Test

--- PatternC#/Program.cs
+++ PatternC#/Program.cs
@@ -43,13 +43,70 @@
 //12345
 //12345
 
-for (int i = 1; i <= 5; i++)
-{
-    for (int j = 1; j <=5; j++)
-    {
-        Console.Write(j);
-    }
-    Console.WriteLine();
-}
+//for (int i = 1; i <= 5; i++)
+//{
+//    for (int j = 1; j <=5; j++)
+//    {
+//        Console.Write(j);
+//    }
+//    Console.WriteLine();
+//}
 
-Console.ReadKey(true);
+//Console.ReadKey(true);
+
+//55555
+//44444
+//33333
+//22222
+//11111
+
+
+//for (int i = 5; i >= 1; i--)
+//{
+//    for (int j = 1; j <=5; j++)
+//    {
+//        Console.Write(i);
+//    }
+
+//    Console.WriteLine();
+//}
+
+//Console.ReadKey(true);
+
+
+
+//54321
+//54321
+//54321
+//54321
+//54321
+
+//for (int i = 5; i >= 1; i--)
+//{
+//    for (int j = 5; j>=1; j--)
+//    {
+//        Console.Write(j);
+//    }
+//    Console.WriteLine();
+//}
+//Console.ReadKey(true);
+
+
+//1 2 3 4 5
+//6 7 8 910
+//1112131415
+//1617181920
+//2122232425
+
+//int k = 1;
+//for (int i = 1; i <= 5; i++)
+//{
+//    for (int j = 1; j <=5; j++)
+//    {
+//        Console.Write("{0,2:D}",k);
+//        k++;
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.ReadKey(true);
