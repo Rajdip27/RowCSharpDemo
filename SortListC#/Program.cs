b7b38c1be61f4d28234f0a6a54191c5d1632ec39
Rajdip27 <srajdip920@gmail.com>
2023-12-09 11:13:14 +0600

    list sorting

---
+++ SortListC#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Channels;
+
+List<int> myList = new List<int> { 1, 5, 3, 6, 8, 7, 6, 2, 4, 9 };
+
+var data = myList.OrderBy(_ => _);
+foreach (var item in data)
+{
+    Console.WriteLine($"{data} ','");
+}
+
+Console.WriteLine();
+Console.ReadKey(true);
+
+ 
