711edee25d08116b9b07632725413eb2804c6df0
Rajdip27 <srajdip920@gmail.com>
2023-12-09 11:05:04 +0600

    OrderBy in Array

---
+++ OderBYC#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+using System.Threading.Channels;
+
+//int[] arr = { 1,7, 2, 4, 3, 6, 5, 8 };
+//var data = arr.OrderBy(_=>_);
+//Console.WriteLine(data);
+
+////foreach (var item in data)
+////{
+////    Console.WriteLine(item);
+////}
+
+
+int[] arr = { 1, 7, 2, 4, 3, 6, 5, 8 };
+var data = arr.OrderBy(_=>_);
+//var data = arr.
+
+foreach (var number in data)
+{
+    Console.Write(number + ",");
+}
+
+Console.WriteLine();
+Console.ReadKey(true);
