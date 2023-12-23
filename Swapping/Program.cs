4e2067af30d9ecf4aae1d3442d613548698505ca
Rajdip27 <srajdip920@gmail.com>
2023-12-23 17:46:32 +0600

    Add Swapping

---
+++ Swapping/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1=10;
+int number2=20;
+int temp=0;
+Console.WriteLine($"Before Number1: {number1}- number2: {number2}");
+Console.WriteLine();
+temp =number1;
+number1=number2;
+number2=temp;
+Console.WriteLine();
+Console.WriteLine($"After Number1: {number1}- number2: {number2}");
+
+Console.ReadKey(true);
