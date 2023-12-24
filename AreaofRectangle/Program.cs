5930bce84aea6665a4315bc5df1530fcc33c66a4
Rajdip27 <srajdip920@gmail.com>
2023-12-24 13:32:59 +0600

    add Area of Rectangle

---
+++ AreaofRectangle/Program.cs
@@ -0,0 +1,5 @@
+﻿int length = 50;
+int breadth = 20;
+int result = length * breadth;
+Console.WriteLine($"Area of Length: {length}  and Breadth: {breadth}  Rectangle is : {result}");
+Console.ReadKey(true);
